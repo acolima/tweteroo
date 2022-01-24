d61da856d77fad7f74d9eb793e5f736ff585dc16
Caroline <anacdolima@gmail.com>
2022-01-23 22:18:55 -0300

    get tweets with page number

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ app.use(cors())
 const users = []
 const tweets = [] 
 
-function latestsTweets(){
-  const latest = []  
-  let cont = 0
-
-  for(let i = tweets.length; cont < 10 && i > 0; cont++, i--)
-    latest.push(tweets[i-1])
-
-  return latest
-}
-
 app.post("/sign-up", (req, res) => {
   const username = req.body.username
   const avatar = req.body.avatar
@@ -34,19 +24,24 @@ app.post("/sign-up", (req, res) => {
 app.post("/tweets", (req, res) => {
   const username = req.headers.user
 
-  if(!req.body.tweet)
+  if(!req.body.tweet || !username)
     res.status(400).send("Todos os campos são obrigatórios!")
   else{
     const userPost = users.find(user => user.username === username)
-    tweets.push({...userPost, tweet: req.body.tweet})
+    tweets.unshift({...userPost, tweet: req.body.tweet})
     res.status(201).send("OK")    
   }
 })
 
 app.get("/tweets", (req, res) => {
-  if(tweets.length === 0)
-    res.send(tweets)
-  else res.send(latestsTweets())
+  const page = req.query.page
+
+  if(!page || page < 1)
+    res.status(400).send("Informe uma página válida!")
+  else{
+    const tweetsPage = tweets.slice((page*10)-10, page*10)
+    res.send(tweetsPage)
+  }
 })
 
 app.get("/tweets/:username", (req, res) => {
